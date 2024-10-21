76b933ef40959e165c9a74b63aa79639dc436ec7
Md Tausif <mdtausifofficial@gmail.com>
2024-10-22 03:32:04 +0530

    issueResolved

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const config = {
+    openWeatherMapToken: your_open_weather_map_token,
+    googleApiKey: your_google_api_key,
+    ipApiKey: your_ip_api_key,
+};
+
+export default config;
