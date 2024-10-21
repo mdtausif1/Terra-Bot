5a8ae5e7729634af60894f8118f9a2ca4ac30f67
Md Tausif <97949757+mdtausif1@users.noreply.github.com>
2024-10-22 03:34:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 const config = {
-    openWeatherMapToken: your_open_weather_map_token,
-    googleApiKey: your_google_api_key,
-    ipApiKey: your_ip_api_key,
+    openWeatherMapToken: '5efc96245a17257ae45388f95bf301b0',
+    googleApiKey: 'AIzaSyD4BG_pvKJ4xApCDoxzvNn-y4-micwI6rs',
+    ipApiKey: 'a31a01378513be',
 };
 
 export default config;
